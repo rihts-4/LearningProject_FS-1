67d7372e1cd7ecfe6e3842a4760b77d75076bfbe
M HAIKAL Muhammad Faiq Haikal <faiqisamazing@gmail.com>
2024-11-01 23:01:39 +0900

    Delete server directory

--- server/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE finance_tracker;
-
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(255) NOT NULL,
-    amount NUMERIC NOT NULL,
-    date DATE NOT NULL
-);
